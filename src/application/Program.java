e91f4b3f1313e159e34378d3f75f8275d258e1b4
mauriciomullerpg <mullerm@uol.com.br>
2020-08-14 14:58:00 -0300

    Department entity class

---
+++ src/application/Program.java
@@ -0,0 +1,17 @@
+package application;
+
+import model.entities.Departament;
+
+public class Program {
+
+	public Program() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		
+		Departament obj =  new Departament(10, "Livros");
+		System.out.println(obj);
+	}
+
+}
